ee49aa6bf62158a0aae6d83d2efde8cb9cab4a72
Ivan Filatov <introvert696@yandex.ru>
2024-12-19 15:35:56 +0300

    create somethink

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,19 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
+
+        dump($request);
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(
             <<<EOF
         
         <html>
             <body>
+            $greet
                 <img src="/images/under-construction.gif" />
             </body>
         </html>
